79609b042def72035fb8b03a6dc523512d442fbb
Murilo Marinho <murilo@nml.t.u-tokyo.ac.jp>
2020-07-07 12:08:14 +0900

    Adding test for issue 18

--- python_issues.py
+++ python_issues.py
@@ -21,6 +21,14 @@ class DQTestCase(unittest.TestCase):
         print("is_unit(x) = {}".format(is_unit(x)))
         print("translation(x) = {}".format(translation(x)))
 
+    # Wrong rotation_axis() and print?
+    def test_python_issue_18(self):
+        r = DQ([0.5068154, -0.8591303, 0.0555768, -0.0440969])
+        r = normalize(r)
+        print('r:', r)
+        print('axis: ', r.rotation_axis())
+        print('angle: ', r.rotation_angle())
+
 
 if __name__ == '__main__':
     unittest.main()
