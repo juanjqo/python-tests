29480605680543fd9e1c9a117019156c6d55cb7e
Murilo Marinho <murilo@nml.t.u-tokyo.ac.jp>
2021-04-12 02:18:21 -0700

    Adding test for dqrobotics/python#26

--- python_issues.py
+++ python_issues.py
@@ -37,5 +37,28 @@ class DQTestCase(unittest.TestCase):
         J = diff_robot.pose_jacobian(q)
         print(J)
 
+    def test_python_issue26(self):
+        import numpy as np
+        from dqrobotics.solvers import DQ_QuadprogSolver
+        from dqrobotics.robot_control import DQ_ClassicQPController
+        from dqrobotics.robot_control import ControlObjective
+        from dqrobotics.robots import KukaLw4Robot
+        import math
+
+        robot = KukaLw4Robot.kinematics()
+        qp_solver = DQ_QuadprogSolver()
+        controller = DQ_ClassicQPController(robot, qp_solver)
+        controller.set_control_objective(ControlObjective.Pose)
+        controller.set_gain(10.0)
+        controller.set_damping(0.001)
+        q = np.array([math.pi, math.pi, math.pi / 2, 0, math.pi, math.pi / 2, 0])
+        effector_p = 0.0 * k_
+        effector_t = 1 + E_ * 0.5 * effector_p
+        effector_phi = -math.pi
+        effector_n = j_
+        effector_r = math.cos(effector_phi / 2) + effector_n * math.sin(effector_phi / 2)
+        effector_pose = effector_t * effector_r
+        print("The control signal is {}.".format(controller.compute_setpoint_control_signal(q, vec8(effector_pose))))
+
 if __name__ == '__main__':
     unittest.main()
