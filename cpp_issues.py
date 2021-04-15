ccbe34778c5da68e5bc3b6b4f9db5ab2c5304431
Murilo Marinho <murilo@nml.t.u-tokyo.ac.jp>
2021-04-14 20:38:57 -0700

    [cpp_issues] Changing to checkAlmostEqual for compatibility with OS besides ubuntu

--- cpp_issues.py
+++ cpp_issues.py
@@ -43,7 +43,7 @@ class DQTestCase(unittest.TestCase):
         # Arbitrary joint values
         q0 = np.array([0.0, 1.7453e-01, 0.0, 1.5708, 0.0, 2.6273e-01, 0.0])
 
-        self.assertEqual(controller.get_task_variable(q0), [0.7716435593916681767723275697790086269378662109375])
+        self.assertAlmostEqual(controller.get_task_variable(q0), [0.7716435593916681767723275697790086269378662109375])
 
 
 if __name__ == '__main__':
