e397971d86de764b62be073266c9a4afa12cb32a
Murilo M. Marinho <murilomarinho@ieee.org>
2023-05-01 10:17:01 +0900

    [all] Updating Murilo's email address.

--- DQ_Kinematics_test.py
+++ DQ_Kinematics_test.py
@@ -16,7 +16,7 @@ This file is part of DQ Robotics.
     along with DQ Robotics.  If not, see <http://www.gnu.org/licenses/>.
 
 Contributors:
-- Murilo M. Marinho (murilo@nml.t.u-tokyo.ac.jp)
+- Murilo M. Marinho (murilomarinho@ieee.org)
 """
 
 import unittest
